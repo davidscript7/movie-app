a45c996d7e13c9c675e3e06fe1b54e258fca67c5
Héctor Gallego <arcane.vortex@outlook.com>
2025-04-06 08:58:55 -0500

    Delete utils directory

--- utils/responsive.js
+++
@@ -1,8 +0,0 @@
-import { Dimensions } from 'react-native';
-
-export const screen = {
-  width: Dimensions.get('window').width,
-  height: Dimensions.get('window').height,
-  isSmallDevice: Dimensions.get('window').width < 375,
-  isTablet: Dimensions.get('window').width >= 768,
-};
